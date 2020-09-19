6b4083a08fdb0953bbf92fdd589cbf54a2d7b13c
mr-lfph <mr-lfph@gmail.com>
2020-09-19 11:29:14 +0800

    Add project files.

---
+++ dotnetcoreapi/Data/HealthContext.cs
@@ -0,0 +1,24 @@
+﻿using dotnetcoreapi.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcoreapi.Data
+{
+    public class HealthContext: DbContext
+    {
+        public HealthContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Ailment> Ailments { get; set; }
+
+        public DbSet<Medication> Medications { get; set; }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
---
+++ dotnetcoreapi/Model/Patient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcoreapi.Model
+{
+    public class Patient
+    {
+        [Key]
+        public int PatientId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Ailment> Ailments{ get; set; }
+
+        public ICollection<Medication> Medications { get; set; }
+    }
+}
